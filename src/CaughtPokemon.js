c6b5247f99580981d3727aed255443de6b52789f
Miladepour <epour.milad@gmail.com>
2022-12-08 08:39:36 +0000

    React Exercises
    -Exercise E

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -4,9 +4,14 @@ const CaughtPokemon = () => {
   let date = new Date().toLocaleDateString();
 
   const [caught, setCaught] = useState(0);
+  const [pokemoncaught, setPokemoncaught] = useState([]);
+
 
   function catchPokemon() {
+    let pokemonArray = ["Butterfree", "Spearow", "Ditto","Pikachu"];
+    const random = Math.floor(Math.random() * pokemoncaught.length);
     setCaught(caught + 1);
+    setPokemoncaught(pokemoncaught.concat(pokemonArray[random]));
   }
 
   return (
@@ -15,6 +20,11 @@ const CaughtPokemon = () => {
         Caught {caught} Pokemon on {date}
       </p>
       <button onClick={catchPokemon}>CaughtPokemon</button>
+      <ul>
+        {pokemoncaught.map((name, index)=> {
+          return <li key={index}>{name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
