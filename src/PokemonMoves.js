854a31261cc4ea4f3cbf1be490f72d1968cabbab
Miladepour <epour.milad@gmail.com>
2022-12-15 11:50:38 +0000

    React 3 Exercise B

--- src/PokemonMoves.js
+++ src/PokemonMoves.js
@@ -1,27 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 function PokemonMoves() {
-   const [pokemonData, setPokemonData] = useState(null);
-   useEffect(()=>{
+  const [pokemonData, setPokemonData] = useState(null);
+  useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/1/")
-    .then((res) => res.json())
-    .then((data) => setPokemonData(data));
-   },[]);
+      .then((res) => res.json())
+      .then((data) => setPokemonData(data));
+  }, []);
 
-  if (pokemonData) {
-    return (
-      <div>
-        <p>{pokemonData.name}'s moves:</p>
-        <ul>
-          {pokemonData.moves.map((move, index) => {
-            return <li key={index}>{move.move.name}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  } else {
-    return null;
-  }
+  return pokemonData ? (
+    <div>
+      <p>{pokemonData.name}'s moves:</p>
+      <ul>
+        {pokemonData.moves.map((move, index) => {
+          return <li key={index}>{move.move.name}</li>;
+        })}
+      </ul>
+    </div>
+  ) : null;
 }
+
 
 export default PokemonMoves;
