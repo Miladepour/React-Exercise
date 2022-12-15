5da6d4b545c70c6962eec9f7e317b9c28dd3d547
Miladepour <epour.milad@gmail.com>
2022-12-15 17:24:31 +0000

    React 3 Exercise Exercise G + Stretch Goal

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -5,20 +5,34 @@ const CaughtPokemon = () => {
 
   const [caught, setCaught] = useState(0);
   const [pokemoncaught, setPokemoncaught] = useState([]);
+  const [pokemonNameInput, setPokemonNameInput] = useState("");
 
 
-  function catchPokemon() {
-    let pokemonArray = ["Butterfree", "Spearow", "Ditto","Pikachu"];
-    const random = Math.floor(Math.random() * pokemoncaught.length);
-    setCaught(caught + 1);
-    setPokemoncaught(pokemoncaught.concat(pokemonArray[random]));
+  function catchPokemon(event) {
+    // let pokemonArray = ["Butterfree", "Spearow", "Ditto","Pikachu"];
+    // const random = Math.floor(Math.random() * pokemoncaught.length);
+    event.preventDefault();
+    if (pokemonNameInput.trim().length !== 0) {
+      console.log("input value is NOT empty");
+      setCaught(caught + 1);
+      setPokemoncaught(pokemoncaught.concat(pokemonNameInput));
+      setPokemonNameInput("");
+    } else {
+      console.log("input value is empty");
+    }
   }
+
+
+  const handleInputChange = (event) => {
+    setPokemonNameInput(event.target.value);
+  };
 
   return (
     <div>
       <p>
         Caught {caught} Pokemon on {date}
       </p>
+      <input type="text" value={pokemonNameInput} onChange={handleInputChange}  />
       <button onClick={catchPokemon}>CaughtPokemon</button>
       <ul>
         {pokemoncaught.map((name, index)=> {
